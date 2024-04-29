64d5ac56905749687eebd66e4de782516db94a2c
Evan Jimenez <evanmji1123@gmail.com>
2024-04-29 15:45:26 -0700

    added layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+
+<body>
+    <slot />
+</body>
+
+
+<style>
+    :root {
+        --bg: #0D1821;
+        --lired: #E85D75;
+        --ligreen: #62C370;
+        --liblue: #3E92CC;
+        --dared: #783744;
+        --dagreen: #356A42;
+        --dablue: #235170;
+    }
+    body {
+        color: white;
+        background-color: var(--bg);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Evan</h1>
+<p><a href="https://kit.svelte.dev" target="_blank">J</a></p>
+<!-- https://coolors.co/palette/153243-1d263b-0d1821-071013-3e92cc-235170-e85d75-783744-62c370-356a42 -->
